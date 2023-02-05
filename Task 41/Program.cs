a718864d2d8c8daaff3b4c67b4adc929a1bdbda3
Andrey <chizhov67@gmail.com>
2023-02-05 15:17:27 +0300

    Task41

---
+++ Task 41/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleAppTask41
+{
+	internal class Program
+	{
+		static void enterArray(int[] array)
+		{
+			for (int i = 0; i < array.Length; i++)
+			{
+				Console.Write("Введите число № " + (i + 1) + " : ");
+				while(!int.TryParse(Console.ReadLine(), out array[i]))
+				{
+					Console.Write("\nНеверный ввод, введите число № " + (i + 1) + ": ");
+				}
+					
+			}
+		}
+
+		static int greaterZero(int [] array)
+		{
+			int count = 0;
+			foreach(int element in array)
+			{
+				if (element > 0)
+				{
+					count++;
+				}
+			}
+
+			return count;
+		}
+
+		static void Main(string[] args)
+		{
+			int M;
+
+			Console.Write("Введите количество чисел которые вы хотите ввести: ");
+
+			while(!int.TryParse(Console.ReadLine(), out M))
+			{
+				Console.Write("\nНеверный ввод, введите число: ");
+			}
+
+			int[] array = new int[M];
+			enterArray(array);
+			Console.WriteLine("\n\nЧисел больше нуля: " + greaterZero(array));
+
+			Console.ReadLine();
+		}
+	}
+}
+
